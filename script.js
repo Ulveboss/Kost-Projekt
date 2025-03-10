1840cfbf4276d114bc08f32b3a5fc7e95e957ccb
Ulveboss <mikkel@iploug.dk>
2025-03-10 16:50:27 +0100

    no work

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-let test = {}
+let test = []
 document.getElementById('submit').addEventListener('click', () => {
     console.log(document.getElementById('searchBar').value)
-    
+    let object = {
+        type: document.getElementById('searchBar').value,
+        amount: document.getElementById('mængde').value + ' g'
+    }
+    test.push(object)
+    console.log(test)
+
+    document.getElementById('seachBar').value = ""
+    document.getElementById('mængde').innerHTML = ""
 })
