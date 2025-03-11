25a94f9eaf07c207d209aaa33e021b7b8c8799ea
Ulveboss <mikkel@iploug.dk>
2025-03-11 10:25:00 +0100

    database upload

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-let test = []
+let test = {
+    mad: []
+}
 document.getElementById('submit').addEventListener('click', () => {
     console.log(document.getElementById('searchBar').value)
     let object = {
         type: document.getElementById('searchBar').value,
         amount: document.getElementById('mængde').value + ' g'
     }
-    test.push(object)
+    test.mad.push(object)
+    //test.push(object)
     console.log(test)
 
-    document.getElementById('seachBar').value = ""
-    document.getElementById('mængde').innerHTML = ""
-})
+    document.getElementById('searchBar').value = ""
+    document.getElementById('mængde').value = ""
+})  
