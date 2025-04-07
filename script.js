1e6ffcb5c0b316bd1d0a6a6fc9580bb005600fbb
Ulveboss <mikkel@iploug.dk>
2025-04-07 13:32:13 +0200

    idfk

--- script.js
+++ script.js
@@ -13,143 +13,134 @@ import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
 fetch('lookUp.json')
 
-    .then(response => response.json())
+  .then(response => response.json())
 
-    .then(data => {
+  .then(data => {
 
-        lookUpData = data;
+    lookUpData = data;
 
-        autocomplete(document.getElementById("myInput"), lookUpData);
-    })
+    autocomplete(document.getElementById("myInput"), lookUpData);
+  })
 
 fetch('database.json')
-    .then(response => response.json())
-    .then(data => {
+  .then(response => response.json())
+  .then(data => {
 
-      database = data
-      pageOpen()
-    } )
+    database = data
+    pageOpen()
+  })
 
 document.getElementById('clear').addEventListener('click', e => {
   localStorage.clear();
   saveState = false;
-  window.location.reload()  
+  window.location.reload()
 })
 
-    document.getElementById('submit').addEventListener('click', e => {
-      let currentFødevare = document.getElementById('myInput').value
-      let index = lookUpData.indexOf(currentFødevare)
+document.getElementById('submit').addEventListener('click', e => {
+  let currentFødevare = document.getElementById('myInput').value
+  let index = lookUpData.indexOf(currentFødevare)
 
-      let fødevare = {
-        foodID: index,
-        amount: document.getElementById('amount').value
-      }
-  
-      
-
-      userData.food.push(fødevare)
-
-      displayFoodList()
-
-      
-      document.getElementById('myInput').value = ""
-      document.getElementById('amount').value = ""
-
-      showData()
-      
-      formUpdate()
-
-    })
+  let fødevare = {
+    foodID: index,
+    amount: document.getElementById('amount').value
+  }
+  userData.food.push(fødevare)
+  displayFoodList()
+  document.getElementById('myInput').value = ""
+  document.getElementById('amount').value = ""
+  showData()
+  formUpdate()
+})
 
 
 function autocomplete(inp, arr) {
   var currentFocus;
-  inp.addEventListener("input", function(e) {
-      var a, b, i, val = this.value;
-      closeAllLists();
-      if (!val) { return false;}
-      currentFocus = -1;
-      a = document.createElement("DIV");
-      a.setAttribute("id", this.id + "autocomplete-list");
-      a.setAttribute("class", "autocomplete-items");
-      this.parentNode.appendChild(a);
-      
-      for (i = 0; i < arr.length; i++) {
-        
-        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-          
-          b = document.createElement("DIV");
-          
-          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
-          b.innerHTML += arr[i].substr(val.length);
-          
-          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
-          
-              b.addEventListener("click", function(e) {
-              
-              inp.value = this.getElementsByTagName("input")[0].value;
-          
-              closeAllLists();
-          });
-          a.appendChild(b);
-        }
+  inp.addEventListener("input", function (e) {
+    var a, b, i, val = this.value;
+    closeAllLists();
+    if (!val) { return false; }
+    currentFocus = -1;
+    a = document.createElement("DIV");
+    a.setAttribute("id", this.id + "autocomplete-list");
+    a.setAttribute("class", "autocomplete-items");
+    this.parentNode.appendChild(a);
+
+    for (i = 0; i < arr.length; i++) {
+
+      if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+
+        b = document.createElement("DIV");
+
+        b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+        b.innerHTML += arr[i].substr(val.length);
+
+        b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+
+        b.addEventListener("click", function (e) {
+
+          inp.value = this.getElementsByTagName("input")[0].value;
+
+          closeAllLists();
+        });
+        a.appendChild(b);
       }
+    }
   });
-  
-  inp.addEventListener("keydown", function(e) {
-      var x = document.getElementById(this.id + "autocomplete-list");
-      if (x) x = x.getElementsByTagName("div");
-      if (e.keyCode == 40) {
-        
-        currentFocus++;
-        
-        addActive(x);
-      } else if (e.keyCode == 38) {
-        currentFocus--;
-        addActive(x);
-      } else if (e.keyCode == 13) {
-       
-        e.preventDefault();
-        if (currentFocus > -1) {
-          if (x) x[currentFocus].click();
-        }
+
+  inp.addEventListener("keydown", function (e) {
+    var x = document.getElementById(this.id + "autocomplete-list");
+    if (x) x = x.getElementsByTagName("div");
+    if (e.keyCode == 40) {
+
+      currentFocus++;
+
+      addActive(x);
+    } else if (e.keyCode == 38) {
+      currentFocus--;
+      addActive(x);
+    } else if (e.keyCode == 13) {
+
+      e.preventDefault();
+      if (currentFocus > -1) {
+        if (x) x[currentFocus].click();
       }
+    }
   });
   function addActive(x) {
     if (!x) return false;
-   
+
     removeActive(x);
     if (currentFocus >= x.length) currentFocus = 0;
     if (currentFocus < 0) currentFocus = (x.length - 1);
-   
+
     x[currentFocus].classList.add("autocomplete-active");
   }
   function removeActive(x) {
-   
+
     for (var i = 0; i < x.length; i++) {
       x[i].classList.remove("autocomplete-active");
     }
   }
   function closeAllLists(elmnt) {
-   
+
     var x = document.getElementsByClassName("autocomplete-items");
     for (var i = 0; i < x.length; i++) {
       if (elmnt != x[i] && elmnt != inp) {
-      x[i].parentNode.removeChild(x[i]);
+        x[i].parentNode.removeChild(x[i]);
+      }
     }
   }
-}
 
-document.addEventListener("click", function (e) {
+  document.addEventListener("click", function (e) {
     closeAllLists(e.target);
-});
+  });
 }
 
 function convert(type) {
   let sum = 0;
-  for(let i = 0; i < userData.food.length; i++){
-   let fødevare = database[userData.food[i].foodID][type]
-    sum +=  fødevare * userData.food[i].amount/100
+  for (let i = 0; i < userData.food.length; i++) {
+    let fødevare = database[userData.food[i].foodID][type]
+    sum += fødevare * userData.food[i].amount / 100
   }
   return sum
 }
@@ -165,16 +156,13 @@ function showData(...types) {
   const pie = d3.pie();
 
   let data = []
-for(let i = 0; i < types.length; i++){
-  if(types[i]){
-    data.push(convert(types[i].name))
+  for (let i = 0; i < types.length; i++) {
+    if (types[i]) {
+      data.push(convert(types[i].name))
+    }
   }
-}
   let arcData = pie(data);
 
-
-
-  
   let g = d3.select('g')
     .selectAll('path')
     .data(arcData)
@@ -182,16 +170,15 @@ for(let i = 0; i < types.length; i++){
     .attr('d', arcGenerator)
     .style('fill', (d, i) => types[i].color);
 
-
   d3.select('g')
     .selectAll('text')
     .data(arcData)
     .join('text')
-    .attr('transform', d => `translate(${arcGenerator.centroid(d)})`) 
-    .attr('text-anchor', 'middle') 
-    .attr('dy', '0.35em') 
-    .style('fill', 'black') 
-    .style('font-size', '14px') 
+    .attr('transform', d => `translate(${arcGenerator.centroid(d)})`)
+    .attr('text-anchor', 'middle')
+    .attr('dy', '0.35em')
+    .style('fill', 'black')
+    .style('font-size', '14px')
     .text((d, i) => types[i].name);
 }
 
@@ -199,18 +186,18 @@ for(let i = 0; i < types.length; i++){
 
 function displayFoodList() {
   const foodList = document.getElementById('foodContainer')
-  while (foodList.firstChild){
+  while (foodList.firstChild) {
     foodList.removeChild(foodList.firstChild)
   }
-  
-  for(let i = 0; i < userData.food.length; i++){
+
+  for (let i = 0; i < userData.food.length; i++) {
     let fødevare = document.createElement('p')
     fødevare.innerHTML = database[userData.food[i].foodID].FødvareNavn + ": " + userData.food[i].amount + " g"
     foodList.appendChild(fødevare)
   }
- 
 
- 
+
+
 }
 
 function showFoodStats(...types) {
@@ -219,31 +206,29 @@ function showFoodStats(...types) {
     div.removeChild(div.firstChild);
   }
 
-  for(let i = 0; i < types.length; i++){
-    if(types[i]){
+  for (let i = 0; i < types.length; i++) {
+    if (types[i]) {
       let paragraph = document.createElement('p')
       let number = convert(types[i])
-      paragraph.innerHTML = `${types[i]}` + ": " + `${Math.round(number,2)}` + ` g `
+      paragraph.innerHTML = `${types[i]}` + ": " + `${Math.round(number, 2)}` + ` g `
       div.appendChild(paragraph)
     }
-   
   }
-
 }
 function pageOpen() {
-  if(userData.food.length > 0){
+  if (userData.food.length > 0) {
     displayFoodList()
-    showData({name: "Protein", color: "yellow"},{name: "Fedt", color: 'red'}, {name: "Tilgængelig kulhydrat", color: 'green'})
+    showData({ name: "Protein", color: "yellow" }, { name: "Fedt", color: 'red' }, { name: "Tilgængelig kulhydrat", color: 'green' })
     showFoodStats("Protein", "Tilgængelig kulhydrat", 'Fedt')
   }
 }
 window.addEventListener('beforeunload', (event) => {
-  if(saveState) localStorage.setItem('foodItems', JSON.stringify(userData))
+  if (saveState) localStorage.setItem('foodItems', JSON.stringify(userData))
 })
 
 window.addEventListener("load", (event) => {
-  if(JSON.parse(localStorage.getItem('foodItems'))){
-    userData = JSON.parse(localStorage.getItem('foodItems'))  
+  if (JSON.parse(localStorage.getItem('foodItems'))) {
+    userData = JSON.parse(localStorage.getItem('foodItems'))
   }
 
 });
@@ -256,11 +241,11 @@ function formUpdate() {
   let form = document.getElementById('checkBoxes')
   let test = form.elements
   let arr = []
-  for(let i = 0; i < checkboxeslength; i++){
-    if(document.getElementById(test[i].id).checked){
+  for (let i = 0; i < checkboxeslength; i++) {
+    if (document.getElementById(test[i].id).checked) {
       let checkbox = document.getElementById(test[i].id)
       let colorvalue = checkbox.getAttribute('data-color')
-      let obj = {name: checkbox.id, color: colorvalue}
+      let obj = { name: checkbox.id, color: colorvalue }
       arr.push(obj)
     }
   }
